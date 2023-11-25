12de035aa9b0eff286084a2e79e9417d7b4ec4db
Samuel255@ <damarisndjebayi.com>
2023-11-25 17:44:40 +0000

    changes on the port number

--- Translator-API/app.py
+++ Translator-API/app.py
@@ -61,6 +61,6 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 1000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.1', port=port)
 
 
