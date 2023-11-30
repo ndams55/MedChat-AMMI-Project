27448a1cb7b6af2c3a5ec720c52a335ba726729c
Samuel255@ <damarisndjebayi.com>
2023-11-30 01:07:17 +0000

    translator app updated

--- Translator-API/app.py
+++ Translator-API/app.py
@@ -1,66 +1,77 @@
-import os
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import Flask, request, jsonify, send_file, render_template, redirect, url_for, send_from_directory
 from PyPDF2 import PdfFileReader, PdfFileWriter
-
-UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
-ALLOWED_EXTENSIONS = {'pdf', 'txt', 'json'}
+from googletrans import Translator
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__, static_url_path="/static")
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# limit upload size upto 8mb
-app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# Configuration
 
+app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+app.config['TRANSLATED_FOLDER'] = os.path.dirname(os.path.abspath(__file__)) + '/translated/'
+
+translator = Translator()
+
+def translate_pdf(input_path, output_path, target_language='en'):
+    try:
+        input_file = PdfFileReader(open(input_path, 'rb'))
+        output = PdfFileWriter()
+
+        translated_content = ""
+
+        for page_number in range(input_file.getNumPages()):
+            page = input_file.getPage(page_number)
+            text = page.extractText()
+            translated_text = translator.translate(text, dest=target_language)
+            translated_content += translated_text.text
+
+            translated_page_writer = open(output_path, 'ab')
+            translated_page_writer.write(translated_text.text.encode('utf-8'))
+            translated_page_writer.close()
+
+        return True, translated_content
+    except Exception as e:
+        print(f"Translation failed: {str(e)}")
+        return False, None
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
+def upload_file():
+    
+    if request == 'POST':
+        print("papapapppa")
         if 'file' not in request.files:
-            print('No file attached in request')
-            return redirect(request.url)
+            return jsonify({'error': 'No file part'})
+        
         file = request.files['file']
+
         if file.filename == '':
-            print('No file selected')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
+            return jsonify({'error': 'No selected file'})
+
+        if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
-            return redirect(url_for('uploaded_file', filename=filename))
+
+            translated_filename = f"translated_{filename}"
+            translated_output_path = os.path.join(app.config['TRANSLATED_FOLDER'], translated_filename)
+            input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            
+            success, translated_content = translate_pdf(input_path, translated_output_path, target_language='fr')
+
+            if success:
+                return redirect(url_for('uploaded_file', filename=filename))
+            #jsonify({'success': 'File uploaded and translated successfully', 'translated_content': translated_content})
+            else:
+                return jsonify({'error': 'Translation failed'})
+            
     return render_template('index.html')
 
 
-def process_file(path, filename):
-    remove_watermark(path, filename)
-    with open(path, 'a') as f:
-       f.write("\nAdded processed content")
+@app.route('/translated/', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(os.path.join(app.config['TRANSLATED_FOLDER'], filename), as_attachment=True)
 
-
-def remove_watermark(path, filename):
-    input_file = PdfFileReader(open(path, 'rb'))
-    output = PdfFileWriter()
-    for page_number in range(input_file.getNumPages()):
-        page = input_file.getPage(page_number)
-        page.mediaBox.lowerLeft = (page.mediaBox.getLowerLeft_x(), 20)
-        output.addPage(page)
-    output_stream = open(app.config['DOWNLOAD_FOLDER'] + filename, 'wb')
-    output.write(output_stream)
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 1000))
     app.run(host='0.0.0.0', port=port)
-
-
